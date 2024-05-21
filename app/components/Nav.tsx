604a953aec55f4f1f29c5fbd686e7a5b692a7a2e
DiegoMarin11 <aether102d@gmail.com>
2024-05-20 20:07:58 -0700

    NavBar updated and added logo

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,19 +6,19 @@ export default function Nav(){
 
     return (
         <div className="mb-14">
-            <div className="z-20 header fixed top-0 left-0 right-0 h-16 bg-gray-900 flex justify-between items-center px-4 ">
-                <div className="logo pl-8">
-                    {/* <img src="/logo.png" alt="Logo" className=" h-10" />  */}
-                </div>
-                <div className="header-container flex justify-end items-center space-x-4 max-w-screen-lg text-14px pr-4 text-white font-termina  font-normal">
-                <a href="#">Trends</a>
-                    <a href="#">Schedule</a>
-                    <a href="#">Location</a>
-                    <a href="#">History</a>
-                    <a href="#">Vacancies</a>
-                    <a href="#">Sponsors</a>
-                </div>
+        <div className="header fixed top-0 left-0 right-0 h-16 bg-gray-900 flex justify-between items-center px-4 ">
+            <div className="logo pl-8">
+                <img src="/logo.png" alt="Logo" className=" h-10" /> 
+            </div>
+            <div className="header-container flex justify-end items-center space-x-4 max-w-screen-lg text-14px pr-4 text-white font-termina  font-normal">
+                <a href="#">Trascendencias</a>
+                <a href="#">Horario</a>
+                <a href="#">Ubicacion</a>
+                <a href="#">Historia</a>
+                <a href="#">Vacantes</a>
+                <a href="#">Patrocinadores</a>
             </div>
         </div>
+    </div>
     )
 }
